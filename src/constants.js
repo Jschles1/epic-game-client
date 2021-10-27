1ce20ec78d554cf28a04b2182f99dc49dae465e7
Jschles1 <jschles1@ramapo.edu>
2021-10-27 19:46:57 -0400

    Initial commit copying base finished project from repl.it

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xe0e167888Aa130CFfFeC2Ea9e523e7dC715f93eA';
+
+const transformCharacterData = (characterData) => {
+    return {
+        name: characterData.name,
+        imageURI: characterData.imageURI,
+        hp: characterData.hp.toNumber(),
+        maxHp: characterData.maxHp.toNumber(),
+        attackDamage: characterData.attackDamage.toNumber(),
+    };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
